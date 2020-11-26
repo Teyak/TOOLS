b234132e4b8fd189d11bbfb52115561efe868f9e
Teyak <74811879+Teyak@users.noreply.github.com>
2020-11-26 10:52:48 +0700

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,282 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo $green
+sleep 0.03
+echo "                 _ "
+sleep 0.03
+echo "                /_)  _.-===-._ "
+sleep 0.03
+echo "               /  .; '_-.'- .'.     _ "
+sleep 0.03
+echo "          __ _/  /.'\ ._  '. , \   (_\ "
+sleep 0.03
+echo "         /\_( \  |.'.-._'- .  '|     \ "
+sleep 0.03
+echo "         |_)  /  |/'    ''--''\|     _\__ "
+sleep 0.03
+echo "         \_)_|  /'  ._.   ._.  '\   / )(_\ "
+sleep 0.03
+echo "         )/  (  \_      _      _/   \' (_| "
+sleep 0.03
+echo "         /   /    \           /     _) (_/ "
+sleep 0.03
+echo "         |   '',_ _'._'---'_.'_ _,.'    | \ "
+sleep 0.03
+echo "         \    /  \ \  '''''  / / /     /   \ "
+sleep 0.03
+echo "          \_.'    \ \       / /  |   .' "
+sleep 0.03
+echo "           '--.,   \ \     / /  _;-'' "
+sleep 0.03
+echo $white"      v2.1"$green"     '|   \ \   / /   | "
+sleep 0.03
+echo "               .;----'''''''----;. "
+sleep 0.03
+echo "              /(                 )\ "
+sleep 0.03
+echo "              \_''-------------''_/ "
+sleep 0.03
+echo "              | '';-----;-----;'' | "
+sleep 0.03
+echo "              | / : \ / : \ / : \ | "
+sleep 0.03
+echo "              |V.','.V.','.V.','.V| "
+sleep 0.03
+echo "              \'./_\.;./_\.;./_\.'/ "
+sleep 0.03
+echo "               ';..___________..;' "
+sleep 0.03
+echo $cyan"        <==============="$yellow"===============>"
+sleep 0.03
+echo $red"        ["$white"÷"$red"] "$yellow"   ["$red"✓"$yellow"]"$white  " Teyak-ID"$yellow  " ["$red"✓"$yellow"]    "$red"["$white"÷"$red"]"
+sleep 0.03
+echo $red"        ["$white"÷"$red"]"$white "https://github.com/Teyak "$red"["$white"÷"$red"]"
+sleep 0.03
+echo $yellow"        <==============="$cyan"===============>"
+sleep 0
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"+"$blue"]"$cyan "SILAHKAN PILIH TOOLS NYA :"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"1"$blue"]"$white" Stabilkan Jaringan v1"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"2"$blue"]"$white" Stabillkan Jaringan v2"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"3"$blue"]"$white" Spam Sms"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"4"$blue"]"$white" Theme Termux"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"5"$blue"]"$white" Spam Call"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"6"$blue"]"$white" HackCode WhatsApp"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"7"$blue"]"$white" Install Kali Linux"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"8"$blue"]"$white" Spammer WhatsApp v1"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"9"$blue"]"$white" Tools virus perusak"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"10"$blue"]"$white" Spam WhatsApp v2"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"11"$blue"]"$white" 370+ install sc hack"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"12"$blue"]"$white" Lacak IP"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"13"$blue"]"$white" Mode Root"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"14"$blue"]"$white" Termux Keys"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"99"$blue"]"$yellow" Edit Tools"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"√"$blue"]"$cyan" INSTALL BAHAN (y/n)"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $blue"["$white"0"$blue"]"$red" exit"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+sleep 0
+echo $white
+read -p "pilih mana╼> = ➤"  bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay TEYAK
+echo $yellow"stabilkan jaringan by TEYAK"
+ping -s1000 1.1.1.1
+fi
+
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then 
+clear
+toilet -f big -F gay TEYAK
+echo $green"stabilkan jaringan by TEYAK"
+ping -s1000 8.8.8.8
+fi
+
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+git clone https://github.com/storiku/noobiespam
+cd noobiespam
+python main.py
+fi
+
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+git clone https://github.com/Zidan-ID17/Theme-Me
+cd Theme-Me
+python2 umbrella.py
+fi
+
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+git clone https://github.com/termuxandi/spamtelphone
+cd spamtelphone
+python Blackphone
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then 
+clear
+git clone https://github.com/Bl4ckDr460n/HxWhatsApp
+cd HxWhatsApp
+ls
+python2 HxWhatsApp.py
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+apt update && apt upgrade
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+pkg install curl
+pkg install wget
+pkg install git
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+git clone https://github.com/storiku/spammerWA
+cd spammerWA
+python2 cwa.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+git clone https://github.com/justahackers/perusak
+cd perusak
+python2 perusak.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+git clone https://github.com/Inject-ID/spam-whatsapp
+cd spam-whatsapp
+bash spam-wa.sh
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+git clone https://github.com/Rajkumrdusad/onex
+cd onex
+chmod +x onex
+sh install
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+git clone https://github.com/hekelpro/ripper
+cd ripper
+python2 ripper.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then 
+clear
+apt update && apt upgrade
+pkg install fakeroot
+fakeroot
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+git clone https://github.com/Zidan-ID17/Termux-Keys
+cd Termux-Keys
+python2 umbrella.py
+fi
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+clear
+git clone https://github.com/Teyak/TOOLS
+cd TOOLS
+nano tools.sh
+fi
+
+if [ $bro = y ] || [ $bro = y ]
+then
+clear
+git clone https://github.com/Teyak/sandi-project 
+cd sandi-project
+sh install.sh
+fi
+
+if [ $bro = n ] || [ $bro = n ]
+then
+clear
+toilet -f big -F gay YAKIN
+echo $yellow"ketikan exit untuk keluar!"
+exit
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $cyan
+figlet "I Love You"
+echo $green
+cd /sdcard
+cd mvp
+cd MIXING
+python2 mengetik.py
+echo $cyan"ketikan exit untuk keluar"
+exit
+fi
